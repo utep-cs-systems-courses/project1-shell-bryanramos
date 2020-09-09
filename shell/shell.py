1cde6e8635c0111e4601631deb0eec061b0047c1
bryanramos <ramos.bryan100@gmail.com>
2020-09-09 14:59:49 -0600

    Shell WIP, added method to fork, exec, wait

--- shell/shell.py
+++ shell/shell.py
@@ -1,6 +1,30 @@
 #! /usr/bin/env python3
 
-import os, sys
+import sys, os
+
+def fork(args):
+    pid = os.getpid()
+    rc = os.fork()
+
+    # based off of p3-exec demo
+
+    if rc < 0: # capture error during fork
+        os.write(2, ("fork failed, returning %d\n" % rc).encode())
+        sys.exit(1)
+
+    elif rc == 0:
+        for dir in re.split(":", os.environ['PATH']): # try each directory in the path
+            program = "%s/%s" % (dir, args[0])
+            try:
+                os.execve(program, args, os.environ) # try to exec program
+            except FileNotFoundError:
+                pass
+                
+        os.write(2, ("%s: command not found\n" % args[0]).encode()) # command not found, print error message
+        sys.exit(1)
+    
+    else:
+        childpid = os.wait()
 
 while True:
     if 'PS1' in os.environ:
@@ -15,9 +39,12 @@ while True:
         except ValueError:
             sys.exit(1)
     
+    args = userInput.split() # tokenize user input arguments
+
     if 'exit' in userInput: # exit command
         break
     elif userInput == "": # for empty input, just reprompt the user
         continue
     else:
+        fork(args)
 
